75953f99b91e2eafa895d43a2e9b7491f4ab23e1
Ewout <lagendijkict@gmail.com>
2021-10-28 10:27:32 +0200

    add slider + several dummy buttons

--- webapp.js
+++ webapp.js
@@ -1,10 +1,29 @@
-let nb = document.getElementById("nettoBedrag");
+//Retrieving DOM elements
+let nettoBedragElem = document.getElementById("nettoBedrag");
+let nettoWinstElem = document.getElementById("nettoWinst");
+let clothesCostElem = document.getElementById("clothesCostEntry")
+let goBtnElem = document.getElementById("goBtn");
 
-function nettoBerekenen() {  
-  let bb = document.getElementById("brutoBedragEntry").value;
-  console.log(nb);
-  console.log(bb);
-  nb.innerText = bb * 0.8;
+//methods
+function nettoStartCalcAndShow() {  
+  let brutoBedrag = document.getElementById("brutoBedragEntry").value;
+  nettoBedragElem.innerHTML = brutoBedrag * 0.8;
 }
 
-nb.addEventListener("click", nettoBerekenen);
+function updateClothesCostDisplay() {
+  let clothesCostEntered = clothesCostElem.value;
+  clothesCostDisplay.innerHTML = clothesCostEntered;
+}
+
+function nettoEndCalcAndShow() {
+  nettoWinstElem.innerHTML = nettoBedragElem.innerHTML - clothesCostElem.value;
+}
+
+//Event Listeners
+nettoBedragElem.addEventListener("click", nettoStartCalcAndShow);
+goBtnElem.addEventListener("click", nettoEndCalcAndShow);
+
+//oninput to make the slider update dynamically
+clothesCostElem.oninput = updateClothesCostDisplay;
+
+
