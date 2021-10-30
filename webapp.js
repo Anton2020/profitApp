5994028116380df25e674d8554993cddfaa745f9
Anton2020 <koningcoding@gmail.com>
2021-10-30 22:23:25 +0200

    Activate slider 2, 3 and 4 + add functionality to receive money advice

--- webapp.js
+++ webapp.js
@@ -2,7 +2,13 @@
 let brutoBedragElem = document.getElementById("brutoBedragEntry");
 let nettoWinstElem = document.getElementById("nettoWinst");
 let clothesCostElem = document.getElementById("clothesCostEntry");
+let makeupCostElem = document.getElementById("makeupCostEntry");
+let decorCostElem = document.getElementById("decorCostEntry");
+let softwareCostElem = document.getElementById("softwareCostEntry");
+
 let hoursWorkElem = document.getElementById("hoursWorkEntry");
+let moneyNextColourElem = document.getElementById("moneyUntilNextColour");
+
 let goBtnElem = document.getElementById("goBtn");
 
 //methods
@@ -11,26 +17,60 @@ function nettoStartCalc() {
 }
 
 function updateClothesCostDisplay() {
-  let clothesCostEntered = clothesCostElem.value;
-  clothesCostDisplay.innerHTML = clothesCostEntered;
+  clothesCostDisplay.innerHTML = clothesCostElem.value;
 }
 
-function nettoEndCalcAndShow() {
-  let netProfitResult = nettoStartCalc() - clothesCostElem.value;
+function updateMakeupCostDisplay() {
+  makeupCostDisplay.innerHTML = makeupCostElem.value;
+}
 
+function updateDecorCostDisplay() {
+  decorCostDisplay.innerHTML = decorCostElem.value;
+}
+
+function updateSoftwareCostDisplay() {
+  softwareCostDisplay.innerHTML = softwareCostElem.value;
+}
+
+function calculateProfit() {
+  let netProfitResult =
+    nettoStartCalc() -
+    clothesCostElem.value -
+    makeupCostElem.value -
+    decorCostElem.value -
+    softwareCostElem.value;
+
+  calculateProfitPerHour(netProfitResult);
+}
+
+function calculateProfitPerHour(netProfitResult) {
   let hoursWork = hoursWorkElem.value;
-  if (netProfitResult / hoursWork < 20) {
+  let netProfitPerHour = netProfitResult / hoursWork;
+  //1.25 calculates the netto money back to bruto, because that's what you start off with
+  let moneyUntilNextColour = Math.ceil(
+    (hoursWork * 30 - netProfitPerHour * hoursWork) * 1.25
+  );
+
+  if (netProfitPerHour < 20) {
     nettoWinstElem.style.color = "red";
-  } else if (netProfitResult / hoursWork < 30) {
+    moneyNextColourElem.style.display = "inline";
+    moneyNextColourElem.innerHTML = `Vraag ${moneyUntilNextColour} euro extra om voldoende winst te maken op deze opdracht.`;
+  } else if (netProfitPerHour < 30) {
     nettoWinstElem.style.color = "orange";
+    moneyNextColourElem.style.display = "inline";
+    moneyNextColourElem.innerHTML = `Vraag ${moneyUntilNextColour} euro extra om voldoende winst te maken op deze opdracht.`;
   } else {
     nettoWinstElem.style.color = "green";
+    moneyNextColourElem.style.display = "none";
   }
-  nettoWinstElem.innerHTML = netProfitResult;
+  nettoWinstElem.innerHTML = Math.floor(netProfitResult);
 }
 
-//Event Listeners
-goBtnElem.addEventListener("click", nettoEndCalcAndShow);
+//Calculate the colour based on the net profit result
+goBtnElem.addEventListener("click", calculateProfit);
 
 //oninput to make the slider update dynamically
 clothesCostElem.oninput = updateClothesCostDisplay;
+makeupCostElem.oninput = updateMakeupCostDisplay;
+decorCostElem.oninput = updateDecorCostDisplay;
+softwareCostElem.oninput = updateSoftwareCostDisplay;
