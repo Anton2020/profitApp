ea7231cb573ae17a1450b9f6fad20ba2d2ea8a26
Ewout Lagendijk <lagendijkict@gmail.com>
2021-11-02 15:59:16 +0100

    Update webapp.js

--- webapp.js
+++ webapp.js
@@ -147,8 +147,8 @@ function prepareConnectionToDB(resultArray) {
   //   }
   // );
 
-  require(['mysql2'], function(mysql) {
-  var connection = mysql.createConnection({
+  require(['mysql2'], function(mysql2) {
+  var connection = mysql2.createConnection({
   host     : 'sql11.freemysqlhosting.net',
   user     : 'sql11448055',
   password : 'DS7RpH5Mtf',
