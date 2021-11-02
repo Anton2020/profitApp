d4a48d633725a04eae423a92485c37153e935027
Ewout Lagendijk <lagendijkict@gmail.com>
2021-11-02 18:52:13 +0100

    Update webapp.js

--- webapp.js
+++ webapp.js
@@ -143,25 +143,7 @@ function connectToDB(resultArray) {
   //   }
   // );
 
-  require(['requirejs'], function (requirejs) {
-});
-
-  var requirejs = require('requirejs');
-
-requirejs.config({
-    //Pass the top-level main.js/index.js require
-    //function to requirejs so that node modules
-    //are loaded relative to the top-level JS file.
-    nodeRequire: require
-});
-
-requirejs(['mysql2'],
-function   (mysql2) {
-    //foo and bar are loaded according to requirejs
-    //config, but if not found, then node's require
-    //is used to load the module.
-});
-  var mysql = require('mysql2');
+  const mysql = require('mysql');
   
   var connection = mysql.createConnection({
   host     : 'sql11.freemysqlhosting.net',
